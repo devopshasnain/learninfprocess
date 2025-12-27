75d0fd85b929fc1c5101e58e7b17b6680147da83
devopshasnain <techtostory@gamil.com>
2025-12-27 18:50:35 +0530

    setup ci workflow and package config

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-console.log("hello bhaiyya");
+const dotenv = require("dotenv")
+dotenv.config()
 
+const express = require("express");
 
+const app = express();
+
+// Azure / cloud friendly port
+const PORT = process.env.PORT || 3000;
+
+// health check (CI + Azure dono ke kaam ka)
+app.get("/health", (req, res) => {
+  res.json({
+    status: "OK",
+    uptime: process.uptime(),
+  });
+});
+
+// home
+app.get("/", (req, res) => {
+  res.send("🚀 Server is running successfully");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
